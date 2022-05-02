81da7bcdddcf50b3598b091da19efb07735a4aa5
Sam Wang <sam.wang.0723@gmail.com>
2022-05-02 10:07:21 +0800

    update Makefile and remove indexex

--- internal/db/migration/20220428215912_remove_unnecessary_index.sql
+++ internal/db/migration/20220428215912_remove_unnecessary_index.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
-ALTER TABLE three_primary 
+ALTER TABLE three_primary
     DROP INDEX `index_stock_id`,
     DROP INDEX `index_dealer`,
     DROP INDEX `index_hedging`;
@@ -8,7 +8,7 @@ ALTER TABLE three_primary
 
 -- +goose Down
 -- +goose StatementBegin
-ALTER TABLE three_primary 
+ALTER TABLE three_primary
     ADD INDEX index_stock_id(stock_id),
     ADD INDEX index_dealer(dealer_trade_shares),
     ADD INDEX index_hedging(hedging_trade_shares);
